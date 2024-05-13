418a748a7d5ba7996057f03c1ea6f0c396ffeeea
Magnus Møland Sønnichsen <magso01@eamv.dk>
2024-05-13 23:30:15 +0200

    Customer Validation, lidt i tvivl om alle skal tilføjes

--- LogicLayer/src/main/java/com/logic/validation/concretes/CustomerValidation.java
+++ LogicLayer/src/main/java/com/logic/validation/concretes/CustomerValidation.java
@@ -4,7 +4,7 @@ import com.logic.handlers.HandlerObject;
 import com.logic.handlers.Request;
 import com.logic.services.enums.ServiceType;
 import com.model.entities.Customer;
-
+//Magnus
 public class CustomerValidation extends HandlerObject{
     public CustomerValidation() {
     }
