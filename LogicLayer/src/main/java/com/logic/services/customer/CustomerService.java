358820223598a6ee9bef5c6a79a3db48a67ef6d7
Magnus Møland Sønnichsen <magso01@eamv.dk>
2024-05-14 12:28:50 +0200

    CustomerService

--- LogicLayer/src/main/java/com/logic/services/customer/CustomerService.java
+++ LogicLayer/src/main/java/com/logic/services/customer/CustomerService.java
@@ -13,7 +13,6 @@ public class CustomerService extends HandlerObject {
     private HandlerHolder holder;
 
     public CustomerService(Data data) {
-        //creates all handlers and adds them to each other
         holder = new SimpleHolder(
                 new SimpleCreateHandler(data),
                 new SimpleReadHandler(data),
@@ -31,7 +30,6 @@ public class CustomerService extends HandlerObject {
 
     @Override
     public void action(Request request) {
-        // go down new COR with CRUD
         holder.query(request);
     }
 }
