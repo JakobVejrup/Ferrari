36610ba7484853e30fc641024b9e1a2853496af0
JakobVejrup <jakob_schjotz@yahoo.dk>
2024-05-30 23:24:40 +0200

    kommentarer

--- DataLayer/src/main/java/com/data/dao/VehicleData.java
+++ DataLayer/src/main/java/com/data/dao/VehicleData.java
@@ -9,26 +9,34 @@ import com.data.ConnectionData;
 import com.data.interfaces.Data;
 import com.model.entities.Vehicle;
 
-// Jakob
+// Jakob DataAccessObeject
 public class VehicleData implements Data {
+// Opstiller reference til ConnectionData
     private ConnectionData db;
+// Laver en db parameter 
     public VehicleData(ConnectionData db) {
         this.db = db;
     }
     @Override
     public Object create (Object parameter) {
+// Opret CallableStatement med db.makeCall
         try (CallableStatement cs = db.makeCall("{call Trade.uspVehicleInsert(?,?,?)}")) {
+// Parameteren castes til vehicle objekt
             Vehicle vehicle = (Vehicle) parameter;
+// De parametre vi har brug for ligger her
             cs.setString("Name", vehicle.getName());
             cs.setDouble("Price", vehicle.getPrice());
             cs.setBytes("Image", vehicle.getImage());
             ResultSet result = cs.executeQuery();
+// Udfører SQL kald til at hente resultaterne
             if (!result.next())
                 return null;
             vehicle.setId(result.getInt("VehicleId"));
+// Hvis resultatet får en vehicle id retuneres dette også
             return vehicle;
         }   
         catch (Exception e) {
+// Skulle der opstå undtagelser retunere den null
             return null;
         }
     }
@@ -36,10 +44,13 @@ public class VehicleData implements Data {
     @Override
     public Object read(Object parameter) {
         try (CallableStatement cs = db.makeCall("{call Trade.uspVehicleGet(?)}")) {
+// Her bliver Id sat som parameter
             cs.setInt("Id",(int)(parameter));
+// Udfører SQL kald ligesom ovenstående, bare kun med 1 parameter
             ResultSet result = cs.executeQuery();
             if (!result.next())
                 return null;
+// Her får vi resultatet på vores vehicle objekt
             return new Vehicle(result.getInt("Id"),
             result.getString("VehicleName"),
             result.getDouble("Price"),
@@ -52,6 +63,7 @@ public class VehicleData implements Data {
 
         @Override
         public Object readAll(Object parameter) {
+// Her vil vi returnere alle vehicles og sortere dem i en arraylist
             ArrayList<Vehicle> vehicles = new ArrayList<Vehicle>();
             try (CallableStatement cs = db.makeCall("{call Trade.uspVehicleGetAll()}")) {
                 ResultSet result = cs.executeQuery();
@@ -70,6 +82,7 @@ public class VehicleData implements Data {
 
         @Override
         public Object update(Object parameter) {
+// Her laver en update vehicle hvor vi caster til vehicle objektet
             try (CallableStatement cs = db.makeCall("{call Trade.uspVehicleUpdate(?, ?, ?, ?)}")) {
                 Vehicle vehicle = (Vehicle) parameter;
                 cs.setInt("Id", vehicle.getId());
@@ -77,14 +90,17 @@ public class VehicleData implements Data {
                 cs.setDouble("Price", vehicle.getPrice());
                 cs.setBytes("Image", vehicle.getImage());
                 cs.execute();
+// Retunerer vehicle som er blevet opdateret hvis getUpdateCount er større end 0
                 return cs.getUpdateCount() > 0 ? vehicle : null;
             }   catch (Exception e) {
+// Ellers returner den null
                 return null;
             }
         }
 
     @Override
     public boolean delete(Object parameter) {
+// Sletter en vehicle ved at få Id'et på vehicle, det sker hvis getUpdateCount er større end 0
         try (CallableStatement cs = db.makeCall("{call Trade.uspVehicleGetAll(?)}")) {
             cs.setInt("Id", ((Vehicle)parameter).getId());
             cs.execute();
--- LogicLayer/src/main/java/com/logic/math/LoanCalculator.java
+++ LogicLayer/src/main/java/com/logic/math/LoanCalculator.java
@@ -10,10 +10,12 @@ import com.rki.bank.InterestRate;
 import com.rki.rki.Rating;
 //jakob
 public class LoanCalculator {
-
+// En invoice indeholder parametre fra Agreement klassen og vi initialisere med en række variabler fra Agreement
     public static Invoice[] låneBeregner(Agreement agreement) {
         Invoice [] Betalinger = new Invoice[agreement.getFixedTerms()];
+// Kalder på renteProcent fra Agreement klassen for at få renten
         double rente = renteProcent(agreement);
+// Her beregnes det samlede lånebeløb ved at tage bilens pris og minus indskuddet
         double totalBeløb = agreement.getVehicle().getPrice() - agreement.getStartValue();
         double månedligBetaling = fastYdelse(totalBeløb, rente, agreement.getFixedTerms());
         int terminer = agreement.getFixedTerms();
@@ -21,21 +23,27 @@ public class LoanCalculator {
         Date firstDate = agreement.getStart();
         double primoGæld = totalBeløb;
         for (int i = 1; i <= terminer; i++) {
+// Her finder vi start datoen for et lån og slut datoen, slutdatoen bliver udregnet ved at plusse antal terminer på start datoen
             LocalDate firstPlus = firstDate.toLocalDate();
+// Konvertere det om til måneder ud fra antal terminer
             firstPlus = firstPlus.plus(1, ChronoUnit.MONTHS);
             Date lastDate = Date.valueOf(firstPlus);
+// Viser hvor meget vi betaler i rente når lånet er slut
             double renten = primoGæld * rente;
             if(i == terminer)
                 månedligBetaling = primoGæld + renten;
+// Afdrag på gæld
             double afdrag = månedligBetaling - renten;
             double ultimoGæld = primoGæld - afdrag;
             Betalinger [i - 1] = new Invoice(agreement, i, firstDate, lastDate, afdrag, renten, ultimoGæld, primoGæld, månedligBetaling, "");
+// Opdatere firstDate og primoGæld for næste iteration
             firstDate = Date.valueOf(firstPlus);
             primoGæld = ultimoGæld;
         }
         return Betalinger;
     }
     public static double renteProcent(Agreement agreement) {
+// Finder renten for Kreditværdigheden og dagsrenten
         double dagsRente = agreement.getDaysRate();
         double RKIværdi = switch(agreement.getRki()) {
             case Rating.A -> 1;
